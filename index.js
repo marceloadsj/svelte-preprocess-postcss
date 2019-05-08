f3eb33deb4e2a78caadd8af9caa46141eeb0b139
Marcelo Junior <marceloadsj1@gmail.com>
2019-05-08 20:27:02 +0200

    Added configPromise inside return to not be stale
    And run prettier to keep code better to read

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const postcssLoadConfig = require(`postcss-load-config`)
-const postcss = require(`postcss`)
+const postcssLoadConfig = require(`postcss-load-config`);
+const postcss = require(`postcss`);
 
 module.exports = (context = {}) => {
-	const { useConfigFile = true, configFilePath, plugins = [], dependencies } = context
+  const {
+    useConfigFile = true,
+    configFilePath,
+    plugins = [],
+    dependencies
+  } = context;
 
-	if (useConfigFile === false) {
-		return ({ content }) => Promise.resolve(process(plugins, content, dependencies))
-	} else {
-		const configPromise = postcssLoadConfig(context, configFilePath)
+  if (useConfigFile === false) {
+    return ({ content }) =>
+      Promise.resolve(process(plugins, content, dependencies));
+  } else {
+    return ({ content }) => {
+      const configPromise = postcssLoadConfig(context, configFilePath);
 
-		return ({ content }) => configPromise.then(
-			({ plugins }) => process(plugins, content, dependencies)
-		)
-	}
-}
+      return configPromise.then(({ plugins }) =>
+        process(plugins, content, dependencies)
+      );
+    };
+  }
+};
 
 function process(plugins, css, dependencies) {
-	return postcss(plugins)
-		.process(css, { from: undefined })
-		.then(code => ({ code, dependencies }))
+  return postcss(plugins)
+    .process(css, { from: undefined })
+    .then(code => ({ code, dependencies }));
 }
