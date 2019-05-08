b950fe969251cb952e1db6734a74c018e244beb6
Marcelo Junior <marceloadsj1@gmail.com>
2019-05-08 19:25:26 +0200

    Adding dependencies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const postcssLoadConfig = require(`postcss-load-config`)
 const postcss = require(`postcss`)
 
 module.exports = (context = {}) => {
-	const { useConfigFile = true, configFilePath, plugins = [] } = context
+	const { useConfigFile = true, configFilePath, plugins = [], dependencies } = context
 
 	if (useConfigFile === false) {
 		return ({ content }) => Promise.resolve(process(plugins, content))
@@ -10,13 +10,13 @@ module.exports = (context = {}) => {
 		const configPromise = postcssLoadConfig(context, configFilePath)
 
 		return ({ content }) => configPromise.then(
-			({ plugins, dependencies }) => process(plugins, content)
+			({ plugins, dependencies }) => process(plugins, content, dependencies)
 		)
 	}
 }
 
-function process(plugins, css) {
+function process(plugins, css, dependencies) {
 	return postcss(plugins)
 		.process(css, { from: undefined })
-		.then(code => ({ code }))
+		.then(code => ({ code, dependencies }))
 }
